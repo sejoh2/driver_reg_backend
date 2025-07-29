94942dc0f34030236378984da47462368f1feb97
joseh33 <josehsejoh33@gmail.com>
2025-07-29 17:51:21 +0300

    Add /notify-driver endpoint

--- index.js
+++ index.js
@@ -6,13 +6,8 @@ require('dotenv').config();
 const axios = require('axios');
 
 const admin = require('firebase-admin');
-const admin = require('firebase-admin');
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK_JSON);
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
 
 //send notification route
 const sendNotification = async (fcmToken, title, body, data = {}) => {
